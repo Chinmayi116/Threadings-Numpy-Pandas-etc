e5f8188f1bb29c86ef61d6e37cde058838f6d720
Chinmayi Patil <chinmayipatil746@gmail.com>
2026-02-09 19:34:18 +0530

    Add files via upload

---
+++ numpy2.py
@@ -0,0 +1,6 @@
+import numpy as np
+zeros_1d=np.zeros(5)
+zeros_2d=np.zeros((3,4))
+
+print(zeros_1d)
+print(zeros_2d)
---
+++ numpy3.py
@@ -0,0 +1,10 @@
+import numpy as np
+range_arr=np.arange(10)
+
+step_arr=np.arange(5,15,2)
+
+print(range_arr)
+print(step_arr)
+
+[0,1,2,3,4,5,6,7,8,9]
+[5,67,9,11,13]
